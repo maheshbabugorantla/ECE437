5e8ec9a007dfda36ebbb0863c816cb1663b8c8ca
maheshbabugorantla <mgorantl@purdue.edu>
2017-08-22 19:05:08 -0400

    Updated the source code for register_file.sv

--- processors/source/register_file.sv
+++ processors/source/register_file.sv
@@ -15,26 +15,23 @@ module register_file
 	input wire clk,
 	input wire n_rst,
 	register_file_if.rf my_rf
-/*	input wire [4:0] wsel,
-	input wire wen,
-	input wire [4:0] rsel_1,
-	input wire [4:0] rsel_2,
-	output reg [31:0] rdat_1,
-	output reg [31:0] rdat_2 */
 );
 
-	word_t [31:0] registerArray; // This is a 32-bit wide Register Array
-	
+	word_t registerArray [31:0]; // This is a 32-bit wide Register Array
+
 	always_ff @(posedge clk, negedge n_rst)
-	begin
+	begin: FLIP_FLOP
 		if(n_rst == 1'b0)
 		begin
-			registerArray = `0;
+			registerArray <= '{default:0};
 		end
-		else
+		else if(my_rf.WEN == 1'b1)
 		begin
-			
+			registerArray[my_rf.wsel] <= my_rf.wdat;
 		end
 	end
-	
+
+	assign my_rf.rdat1 = registerArray[my_rf.rsel1];
+	assign my_rf.rdat2 = registerArray[my_rf.rsel2];
+
 endmodule
