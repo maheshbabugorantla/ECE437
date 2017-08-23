98b54585fd8952399ca415543c187ef2d7f2cc4a
maheshbabugorantla <mgorantl@purdue.edu>
2017-08-22 22:52:11 -0400

    Added alu_if.vh:ALU Interface, Added mapped/* files, Added source/alu.sv

---
+++ processors/source/alu.sv
@@ -0,0 +1,27 @@
+/*
+	Author: Mahesh Babu Gorantla, mgorantl@purdue.edu
+	mg Account: mg226
+	module: alu
+	Description: This source code is used to design the alu for Lab-1 => This is a purely Combinational Block
+*/
+
+`include "../include/cpu_types_pkg.vh"
+`include "../include/alu_if.vh"
+
+import cpu_types_pkg::*;
+
+module alu(
+	alu_if.alu my_alu
+);
+
+	always_comb
+	begin: OPERATION
+		
+	end
+
+	always_comb
+	begin: FLAGS
+
+	end	
+
+endmodule
